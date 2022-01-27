ce904582da64606c45388bdaa91b1f1ba8aa6693
juanfeReyes <juan.felipe.reyesg@gmail.com>
2022-01-27 17:26:36 -0500

    first commit

---
+++ src/main/java/com/roadToMaster/UniversityManagerApi/UniversityManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.roadToMaster.UniversityManagerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roadToMaster/UniversityManagerApi/UniversityManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadToMaster.UniversityManagerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
