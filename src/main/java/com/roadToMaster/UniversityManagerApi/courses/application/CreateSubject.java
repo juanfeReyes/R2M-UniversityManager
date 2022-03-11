259180c9eb457fe9b51a1e6140372e728307d50e
juanfeReyes <juan.felipe.reyesg@gmail.com>
2022-03-10 22:26:45 -0500

    lint fix

--- src/main/java/com/roadToMaster/UniversityManagerApi/courses/application/CreateSubject.java
+++ src/main/java/com/roadToMaster/UniversityManagerApi/courses/application/CreateSubject.java
@@ -1,5 +1,6 @@
 package com.roadToMaster.UniversityManagerApi.courses.application;
 
+import com.roadToMaster.UniversityManagerApi.courses.domain.Course;
 import com.roadToMaster.UniversityManagerApi.courses.domain.Schedule;
 import com.roadToMaster.UniversityManagerApi.courses.domain.Subject;
 import com.roadToMaster.UniversityManagerApi.courses.domain.exceptions.ScheduleConflictException;
@@ -9,6 +10,7 @@ import com.roadToMaster.UniversityManagerApi.courses.infrastrucure.persistence.e
 import com.roadToMaster.UniversityManagerApi.courses.infrastrucure.persistence.entity.SubjectEntity;
 import com.roadToMaster.UniversityManagerApi.shared.domain.exceptions.ResourceAlreadyCreatedException;
 import com.roadToMaster.UniversityManagerApi.shared.domain.exceptions.ResourceNotFoundException;
+import com.roadToMaster.UniversityManagerApi.users.domain.User;
 import com.roadToMaster.UniversityManagerApi.users.infrastructure.persistence.UserRepository;
 import com.roadToMaster.UniversityManagerApi.users.infrastructure.persistence.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,7 +36,6 @@ public class CreateSubject implements ICreateSubject {
     this.subjectRepository = subjectRepository;
   }
 
-  //TODO: Clean this monster
   @Transactional
   public Subject execute(String id, String name, String description, String courseName,
                          String professorUsername, List<Schedule> schedules) {
@@ -42,40 +43,38 @@ public class CreateSubject implements ICreateSubject {
       throw new ResourceAlreadyCreatedException(String.format("Subject already exists with id: %s", id));
     }
 
-    var uax = courseRepository.findAll();
     var courseEntity = courseRepository.findByName(courseName);
     if (courseEntity.isEmpty()) {
       throw new ResourceNotFoundException(String.format("Course with name %s does not exists", courseName));
     }
     var course = CourseEntity.toDomain(courseEntity.get());
 
-    //Search user must have rol professor
     var user = userRepository.findByUsername(professorUsername);
     if (user.isEmpty()) {
       throw new ResourceNotFoundException(String.format("Professor with username: %s was not found", professorUsername));
     }
+
     var professor = UserEntity.toDomain(user.get());
+    var professorSchedules = getProfessorSchedules(professor, course);
 
-    //TODO: this should be at infrastructure level
-    //Find all subjects of professor and extract schedules
-    var professorSubjects = subjectRepository.findByProfessorUsername(professor.getUsername())
-        .stream().map(subject -> SubjectEntity.toDomain(subject, course)).collect(Collectors.toList());
-    var professorSchedules = professorSubjects.stream()
-        .flatMap(subject -> subject.getSchedules().stream())
-        .collect(Collectors.toList());
-
-    //Grant that current schedules does not overlap with professor schedules
     var overlappedSchedules = computeOverlappedSchedules(schedules, professorSchedules);
 
     if (!overlappedSchedules.isEmpty()) {
       throw new ScheduleConflictException("Cannot create subject schedules overlap with professors schedules", overlappedSchedules);
     }
 
-    //Add subjects
     var subject = new Subject(id, name, description, course, schedules, professor);
     subjectRepository.save(SubjectEntity.toEntity(subject, courseEntity.get()));
 
     return subject;
+  }
+
+  private List<Schedule> getProfessorSchedules(User professor, Course course){
+    var professorSubjects = subjectRepository.findByProfessorUsername(professor.getUsername())
+        .stream().map(subject -> SubjectEntity.toDomain(subject, course)).collect(Collectors.toList());
+    return professorSubjects.stream()
+        .flatMap(subject -> subject.getSchedules().stream())
+        .collect(Collectors.toList());
   }
 
   private List<Schedule> computeOverlappedSchedules(List<Schedule> newSchedules, List<Schedule> oldSchedules) {
