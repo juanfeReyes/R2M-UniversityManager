b29b36142b477dfc6463fb766a8603f6af359512
juanfeReyes <juan.felipe.reyesg@gmail.com>
2022-04-13 16:52:31 -0500

    feature: delete course given id

---
+++ src/main/java/com/roadToMaster/UniversityManagerApi/courses/infrastrucure/api/DeleteCourseController.java
@@ -0,0 +1,32 @@
+package com.roadToMaster.UniversityManagerApi.courses.infrastrucure.api;
+
+import com.roadToMaster.UniversityManagerApi.courses.application.IDeleteCourse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("course")
+@Tag(name = "Course")
+@Validated
+@SecurityRequirement(name = "basicAuth")
+public class DeleteCourseController {
+
+  private final IDeleteCourse deleteCourse;
+
+  @Autowired
+  public DeleteCourseController(IDeleteCourse deleteCourse) {
+    this.deleteCourse = deleteCourse;
+  }
+
+  @Operation(summary = "Delete course by Id", security = {@SecurityRequirement(name = "OAuthScheme")})
+  @DeleteMapping (value = "/{courseId}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public void deleteCourse(@PathVariable String courseId){
+    deleteCourse.execute(courseId);
+  }
+
+}
