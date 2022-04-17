13fcd335d70aedb0024f00c9aae9d9cda0241cea
juanfeReyes <92064684+juanfeReyes@users.noreply.github.com>
2022-04-17 14:57:49 -0500

    Fix/improve schedule collition validation (#17)
    * fixed aws credentials injection
    
    * fix overlapped schedules computation

--- src/main/java/com/roadToMaster/UniversityManagerApi/courses/domain/Schedule.java
+++ src/main/java/com/roadToMaster/UniversityManagerApi/courses/domain/Schedule.java
@@ -1,18 +1,23 @@
 package com.roadToMaster.UniversityManagerApi.courses.domain;
 
+import com.roadToMaster.UniversityManagerApi.shared.domain.exceptions.ResourceConflictException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.apache.tomcat.jni.Local;
 
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
 
 @Getter
-@AllArgsConstructor
 public class Schedule {
-
-  //TODO add validation that endDate > startDate
 
   private String id;
 
@@ -24,24 +29,43 @@ public class Schedule {
 
   private String description;
 
+  public Schedule(String id, DayEnum day, LocalTime startTime, LocalTime endTime, String description){
+    this.id = id;
+    this.day = day;
+    this.startTime = startTime;
+    this.endTime = endTime.minusMinutes(1);
+    this.description = description;
+
+    if(endTime.isAfter(startTime)){
+      throw new ResourceConflictException("Schedule end time should be after start time");
+    }
+  }
+  
   public static List<Schedule> computeOverlappedSchedules(List<Schedule> newSchedules, List<Schedule> oldSchedules) {
-    return newSchedules.stream()
-        .filter(schedule -> !oldSchedules.stream()
-            .filter(oldSchedule -> oldSchedule.isScheduleOverlapped(schedule))
-            .collect(Collectors.toList()).isEmpty())
-        .collect(Collectors.toList());
+    var conflictedSchedules = new ArrayList<Schedule>();
+
+    Map<DayEnum, List<Schedule>> schedulesByDay = Stream
+        .concat(oldSchedules.stream(), newSchedules.stream())
+        .collect(groupingBy(Schedule::getDay));
+    // for each day sort by start date
+
+    schedulesByDay.keySet().stream().forEach((day) -> {
+      var schedulesDay = schedulesByDay.get(day);
+      schedulesDay.sort(Comparator.comparing(s -> s.startTime));
+      for(int i = 0; i < schedulesDay.size()-1; i++){
+        if(schedulesDay.get(i).isScheduleOverlapped(schedulesDay.get(i+1))){
+          conflictedSchedules.add(schedulesDay.get(i));
+        }
+      }
+    });
+
+    return conflictedSchedules;
   }
 
-  public boolean isScheduleOverlapped(Schedule newSchedule) {
-    if (!this.day.equals(newSchedule.day)) {
-      return false;
-    }
-    var startHours = startTime.truncatedTo(ChronoUnit.HOURS);
+  private boolean isScheduleOverlapped(Schedule newSchedule) {
     var endHours = endTime.truncatedTo(ChronoUnit.HOURS);
     var newStartHours = newSchedule.startTime;
-    var newEndHours = newSchedule.endTime;
 
-    return (startHours.equals(newStartHours) || startHours.isAfter(newStartHours) && startHours.isBefore(newEndHours)) ||
-        (endHours.isAfter(newStartHours) && endHours.isBefore(newEndHours) || endHours.equals(newEndHours));
+    return endHours.isAfter(newStartHours);
   }
 }
